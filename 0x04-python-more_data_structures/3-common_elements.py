69dc390f6ffc0f3c4ab3804d5a9b1f01bf0f897d
Edwin-Paul <github.paul01@gmail.com>
2022-09-29 12:52:21 -0700

    More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 0-square_matrix_simple.py
+# Edd
+
+def common_elements(set_1, set_2):
+    """Return a set of common elements in two sets."""
+    return (set_1 & set_2)
---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 0-square_matrix_simple.py
+# Edd
+
+def number_keys(a_dictionary):
+    """Return the number of keys in a dictionary."""
+    return (len(a_dictionary))
